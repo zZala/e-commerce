69519520ea763c579d5456293120c4d7b63321ed
zzala <lazzarin.andrea03@gmail.com>
2022-06-23 15:58:36 +0200

    titles

--- ecommerce/checkout.php
+++ ecommerce/checkout.php
@@ -8,7 +8,7 @@ session_start();
 
 <head>
     <meta charset="utf-8">
-    <title>E Store - eCommerce HTML Template</title>
+    <title>Checkout</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="eCommerce HTML Template Free Download" name="keywords">
     <meta content="eCommerce HTML Template Free Download" name="description">
